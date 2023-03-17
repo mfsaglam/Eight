780b95e80ecfdb48d5828c9379aa9480acedd59d
mfsaglam <mfsaglam1@gmail.com>
2023-03-17 18:21:52 +0300

    add `CreateAccountViewController`

---
+++ Eight/CreateAccountViewController.swift
@@ -0,0 +1,17 @@
+//
+//  CreateAccountViewController.swift
+//  Eight
+//
+//  Created by Fatih Sağlam on 17.03.2023.
+//
+
+import UIKit
+
+class CreateAccountViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
