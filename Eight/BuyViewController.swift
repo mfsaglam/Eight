efca382ccf6cb1c09ca3de1209f0ff417c7d6265
Fatih Saglam <fatih.saglam@adesso.com.tr>
2023-03-17 15:42:58 +0300

    create `BuyViewController`

---
+++ Eight/BuyViewController.swift
@@ -0,0 +1,17 @@
+//
+//  BuyViewController.swift
+//  Eight
+//
+//  Created by Saglam, Fatih on 17.03.2023.
+//
+
+import UIKit
+
+class BuyViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
